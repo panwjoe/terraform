775359b6d67f4277b7fe931d48dc2dabaa37247b
panwjoe <99195980+panwjoe@users.noreply.github.com>
2022-02-07 12:51:19 -0500

    Update s3.tf
    Co-authored-by: prisma-cloud-devsecops[bot] <89982750+prisma-cloud-devsecops[bot]@users.noreply.github.com>

--- s3.tf
+++ s3.tf
@@ -122,6 +122,13 @@ resource "aws_s3_bucket" "data_science" {
       }
     }
   }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 }
 
 resource "aws_s3_bucket" "logs" {
